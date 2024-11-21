56ac8db465454f58172f8efbd5793a3f23f4385a
Dhanush B <talktodhanush4554@gmail.com>
2024-11-21 13:46:12 +0530

    hosting error debug try 1

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const path = require("path");
 const fs = require("fs");
 
 const app = express();
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(cors());
@@ -81,6 +81,6 @@ app.use((err, req, res, next) => {
 });
 
 // Start the server
-app.listen(PORT, () => {
+app.listen("PORT", () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
